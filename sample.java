a36881565b8a17dcb800b14973214040b3d95fe1
sowa-takei <souwasora@gmail.com>
2022-02-05 05:14:04 +0000

    java 勉強開始

--- sample.java
+++ sample.java
@@ -1,1 +1,11 @@
-public
+public class sample {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i <= 10; i++){
+            if (i % 2 == 0){
+            count += 1;
+            }
+        }
+        System.out.println(count);
+    }
+}
