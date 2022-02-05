97427d4e71fcdd776df733cc1fb16dc77712ee19
sowa-takei <souwasora@gmail.com>
2022-02-05 08:08:46 +0000

    配列

---
+++ name.java
@@ -0,0 +1,8 @@
+public class name {
+  public static void main(String[] args){
+    String[] name = {"竹井","田島","多田"};
+    for (int i = 0; i < 3; i++){
+      System.out.println(name[i]);
+    }
+  }
+}
