5776f816f724df85e305267d1d715decfcf4a442
sowa-takei <souwasora@gmail.com>
2022-02-05 05:38:55 +0000

    add jaa

---
+++ even.java
@@ -0,0 +1,9 @@
+public class even {
+  public static void main(String[] srgs){
+    for(int i = 1; i <= 10; i++){
+      if(i % 2 == 0){
+       System.out.println(i); 
+      }
+    }
+  }
+}
--- sample.java
+++ sample.java
@@ -1,11 +1,13 @@
 public class sample {
     public static void main(String[] args) {
         int count = 0;
+        int sum = 0;
         for (int i = 1; i <= 10; i++){
             if (i % 2 == 0){
             count += 1;
+            sum += i;
             }
         }
-        System.out.println(count);
+        System.out.println("偶数の合計は" + sum +"です。また偶数の数は" + count + "です。");
     }
 }
