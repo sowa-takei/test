f51e9db66ef082143d3049a65a1d68e382bd3dbc
sowa-takei <souwasora@gmail.com>
2022-02-06 06:12:58 +0000

    追加

--- name.java
+++ name.java
@@ -1,8 +1,8 @@
 public class name {
-  public static void main(String[] args){
+  public static void main (String[] args) {
     String[] name = {"竹井","田島","多田"};
     for (int i = 0; i < 3; i++){
-      System.out.println(name[i]);
+      System.out.println(name[i]+",");
     }
   }
 }
--- sample.java
+++ sample.java
@@ -3,11 +3,11 @@ public class sample {
         int count = 0;
         int sum = 0;
         for (int i = 1; i <= 10; i++){
-            if (i % 2 == 0){
-            count += 1;
-            sum += i;
+            if(i % 2 == 0){
+                sum += i;
+                count += 1;
             }
         }
-        System.out.println("偶数の合計は" + sum +"です。また偶数の数は" + count + "です。");
+        System.out.println("偶数の合計は" + sum +"です。また、偶数の数は" + count + "です");
     }
 }
