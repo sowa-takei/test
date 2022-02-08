5ee2fbe2fb3d240a5c3f98b6b1d2803a04698ab8
sowa-takei <souwasora@gmail.com>
2022-02-08 03:48:33 +0000

    最初の数字の定義実施

--- dentak.java
+++ dentak.java
@@ -1,3 +1,15 @@
 public class dentak {
-  public static void main (String[] args);
+  public static void main (String[] args){
+    // 最初の数字定義
+    int val1 = 0;
+    // 次の数字
+    int val2 = 0;
+    // 演算子 
+    String ope;
+    
+    if(args.length != 3){
+      System.out.println("usage :java dentak 数値　演算子　数値");
+      System.out.println("演算子は kake waru tasu hikuの4つです");
+    }
+  }
 }
