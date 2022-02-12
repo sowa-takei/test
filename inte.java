1159941409e2a3e7cb73e4c8d11340e4bd6f31f0
sowa-takei <souwasora@gmail.com>
2022-02-12 10:11:21 +0000

    修正実施

--- inte.java
+++ inte.java
@@ -1,7 +1,9 @@
 public class inte {
   public static void main(String[] args){
-    for(int i = 0; i < 20; i += 5){
-      System.out.println(i);
+    int number[] = new int[20];
+    for(int i = 0; i <= 18; i ++ ){
+      number[i + 1] = number[i] + 5;
+      System.out.println(number[i]);
     }
   }
 }
