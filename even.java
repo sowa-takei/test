3147955bb255f714c32d52752f1db5d572f47c8b
sowa-takei <souwasora@gmail.com>
2022-02-13 14:02:04 +0000

    追加

--- even.java
+++ even.java
@@ -1,7 +1,7 @@
 public class even {
   public static void main(String[] srgs){
     for(int i = 1; i <= 10; i++){
-      if(i % 2 == 0){
+      if(i % 2 != 0){
        System.out.println(i); 
       }
     }
--- inte.java
+++ inte.java
@@ -1,9 +1,28 @@
 public class inte {
   public static void main(String[] args){
     int number[] = new int[20];
-    for(int i = 0; i <= 18; i ++ ){
-      number[i + 1] = number[i] + 5;
-      System.out.println(number[i]);
+    // 入力する処理
+    for(int i = 0; i < 20; i ++ ){
+       int number2 = (i + 1) * 5;
+       number[i] = number2;
+    }
+    // 出力する処理
+    for(int i = 0; i < 20; i++){
+      if(i <= 10){
+        if(number[i] % 2 != 0){
+          System.out.print(number[i] + ",");
+        }else {
+          System.out.print(number[i]);
+        }
+      }
+      if(i >= 11 && i < 20 ){
+        if(number[i] % 2 != 0){
+        System.out.print(number[i]); 
+      }else {
+        System.out.print(number[i] + ",");
+      }
     }
   }
+      // System.out.println(number[i]);
+}
 }
