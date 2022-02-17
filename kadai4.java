8f4ea5ac224f4b20bee79034f32932f8c83539ad
sowa-takei <souwasora@gmail.com>
2022-02-17 03:54:11 +0000

    説明追加

--- kadai4.java
+++ kadai4.java
@@ -3,23 +3,30 @@ public class kadai4 {
     // 入力　配列定義
     int num [] = new int[20];
     for(int i = 0; i < num.length; i++){
-      
+      // for文の初期値0に１を足して*5をしていく0～19
       num[i] = (i + 1) * 5;
     }
     
-    
+    // 出力　for文
     for(int i = 0; i < num.length; i++){
+      // 10まではtrue
       if(i <= 10){
+        // 割り切れなかったら
        if(num[i] % 2 != 0){
+        // 奇数の後に,をつける
         System.out.print(num[i] + ",");
        }else {
+        // 偶数はそのまま出力
         System.out.print(num[i]);
        }
       }
-      if(i > 10 && i <= 20){
+      // 10~19
+      if(i >= 11 && i < 20){
         if(num[i] % 2 != 0){
+          // 奇数はそのまま出力
           System.out.print(num[i]);
         }else {
+          // 偶数の後に,をつける
           System.out.print(num[i] + ",");
         }
       }
