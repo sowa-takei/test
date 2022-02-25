f410fdfa8fb4f6878a1c54fe23f7df0811112dc5
sowa-takei <souwasora@gmail.com>
2022-02-25 03:54:36 +0000

    ピラミッド作成

---
+++ test7.java
@@ -0,0 +1,9 @@
+public class test7 {
+  public static void main(String[] args){
+    for(int i = 0; i < 5; i++){
+      for(int k= 0; k < (5 - i); k++){
+        System.out.print(" ");
+      }
+    }
+  }
+}
