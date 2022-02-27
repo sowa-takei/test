88fb71015f3d12d6d1f473fe12bc70c372839700
sowa-takei <souwasora@gmail.com>
2022-02-27 13:19:24 +0000

    練習う

--- test7.java
+++ test7.java
@@ -1,9 +1,13 @@
 public class test7 {
   public static void main(String[] args){
     for(int i = 0; i < 5; i++){
-      for(int k= 0; k < (5 - i); k++){
+      for(int k= 0; k < 5-(i + 1); k++){
         System.out.print(" ");
-      }
+        }
+        for(int j= 0; j < (i + 1)*2-1; j++){
+          System.out.print(" * ");
+        }
+      System.out.println("\n");
     }
   }
 }
