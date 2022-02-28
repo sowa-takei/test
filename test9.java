4b2537f6faef8ecf5041089fb667054e315ed4e8
sowa-takei <souwasora@gmail.com>
2022-02-28 03:46:36 +0000

    練習

---
+++ test9.java
@@ -0,0 +1,12 @@
+// Scannerクラスを使うためのimport文
+import java.util.Scanner;
+public class test9 {
+  public static void main(String[] args){
+    int num1 = 
+    for(int i = 1; i <= 27; i++){
+      if(i % 3 == 0){
+       System.out.println(i); 
+      }
+    }
+  }
+}
