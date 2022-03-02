009ceede4a30ea657482ac21357813fb4f210c4b
sowa-takei <souwasora@gmail.com>
2022-03-02 12:58:53 +0000

    最大値、最小値

--- test11.java
+++ test11.java
@@ -3,13 +3,26 @@ public class test11 {
   public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
     int max_value = 0;
+    int min_value = 0;
+    System.out.println("数値を入力してください");
     for(int i = 0; i < 10; i++){
+      
       int value = scanner.nextInt();
+      if(i == 0){
+        min_value = value;
+        max_value = value;
+      }
+      
       if(value > max_value){
         max_value = value;
       }
+      
+      if(value < min_value){
+        min_value = value;
+      }
     }
-    System.out.print("最大値は" + max_value + "です");
+    System.out.println("最大値は" + max_value + "です");
+    System.out.println("最小値は" + min_value + "です");
     scanner.close();
   }
 }
